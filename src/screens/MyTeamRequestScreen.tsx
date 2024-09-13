f3256fe8eaeb18771062eb7a3d809ac32abe015c
tatahyeonv <xhrl523@naver.com>
2024-09-13 23:32:36 +0900

    feat: TeamEditScreen 제작

--- src/screens/MyTeamRequestScreen.tsx
+++ src/screens/MyTeamRequestScreen.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { ScrollView, View, Text, Dimensions, TouchableOpacity, StyleSheet, FlatList } from 'react-native';
+import { ScrollView, View, Dimensions, StyleSheet } from 'react-native';
 import TeamItem from '@components/MyTeam/TeamItem';
 
 const { width } = Dimensions.get('window');
@@ -9,22 +9,6 @@ const MyTeamRequestScreen = () => {
 
   return (
     <View className="flex-1 bg-black100">
-      <View className="h-[70px] px-4 items-center">
-        <View className="w-full h-[54px] space-x-3 p-[8px] flex flex-row rounded-full bg-white border-[1px] border-black200">
-          <TouchableOpacity
-            onPress={() => setIsAllTeam(true)}
-            style={styles.button}
-            className={`${isAllTeam ? 'bg-black900' : 'bg-white'}  rounded-full w-full h-full flex justify-center items-center`}>
-            <Text className={`${isAllTeam ? 'text-white' : 'text-black500'} font-PTDSemiBold text-sm`}>모든 모임</Text>
-          </TouchableOpacity>
-          <TouchableOpacity
-            onPress={() => setIsAllTeam(false)}
-            style={styles.button}
-            className={`${!isAllTeam ? 'bg-black900' : 'bg-white'} rounded-full w-full h-full flex justify-center items-center`}>
-            <Text className={`${!isAllTeam ? 'text-white' : 'text-black500'} font-PTDSemiBold text-sm`}>받은 모임 요청</Text>
-          </TouchableOpacity>
-        </View>
-      </View>
       <ScrollView className="px-4 pt-[4px]" contentContainerStyle={{ paddingBottom: 30 }}>
         <View className="bg-white rounded-2xl border-[1px] border-black200 overflow-hidden">
           {[...Array(20)].map((_, idx) => (
--- src/screens/TeamEditScreen.tsx
+++ src/screens/TeamEditScreen.tsx
@@ -1,10 +1,55 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { View } from 'react-native';
+import useCurrentRoute from '@hooks/useCurrentRoute';
+import { useForm } from 'react-hook-form';
+import InputField from '@components/common/InputField';
+import Button from '@components/common/Button';
+import useNavi from '@hooks/useNavi';
 
-const TeamEditScreen = () => {
+interface Form {
+  teamName: string;
+}
+
+interface Props {
+  paramName: string;
+}
+
+const TeamEditScreen = ({}: Props) => {
+  const { navigation } = useNavi();
+  const { route } = useCurrentRoute();
+  console.log('route:', route.params.id);
+
+  const {
+    control,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<Form>();
+
+  const onSubmit = handleSubmit(async data => {
+    const { teamName } = data;
+    console.log('teamName:', teamName);
+    if (errors.teamName) return;
+    navigation.navigate('TeamSetting', { id: route.params.id });
+  });
+
   return (
-    <View>
-      <Text>TeamEditScreen</Text>
+    <View className="px-4 pb-5 flex justify-between h-full">
+      <View className="h-[122px] flex justify-center">
+        <InputField
+          label="모임 이름"
+          name="teamName"
+          control={control}
+          rules={{
+            required: '필수 입력 항목입니다',
+            maxLength: { value: 20, message: '20자 이내로 설정해 주세요.' },
+          }}
+          errors={errors}
+          placeholder="모임 이름을 입력해 주세요"
+        />
+      </View>
+      <Button onPress={onSubmit} disabled={!!errors.teamName}>
+        수정 완료하기
+      </Button>
     </View>
   );
 };
